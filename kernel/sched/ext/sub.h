80f45e3ee8690d76cd470c512fe5d1bb412a6fa7
Andrea Righi <arighi@nvidia.com>
2026-08-05 10:23:47 +0200

    sched_ext: Move reject DSQ draining into core
    The reject DSQ drain will be generalized for placement failures outside
    sub-scheduler capability rejection. Move the drain into the core
    sched_ext implementation first so the subsequent behavioral changes can
    be reviewed separately. No functional change.
    
    Signed-off-by: Andrea Righi <arighi@nvidia.com>

--- kernel/sched/ext/sub.h
+++ kernel/sched/ext/sub.h
@@ -37,7 +37,6 @@ void scx_discard_stale_ecaps_syncs(void);
 struct scx_dispatch_q *__scx_resolve_local_dsq(struct scx_sched *sch, struct rq *rq,
 					       struct task_struct *p, u64 *enq_flags);
 bool scx_task_reenq_on_cap_revoke(struct rq *rq, struct task_struct *p);
-void __scx_reenq_reject(struct rq *rq);
 void scx_rescue_charge(struct rq *rq, s64 delta_exec);
 void scx_rescue_end(struct rq *rq);
 bool scx_rescue_keep(struct rq *rq, struct task_struct *p);
@@ -91,14 +90,6 @@ static inline struct scx_dispatch_q *scx_resolve_local_dsq(struct scx_sched *sch
 	return __scx_resolve_local_dsq(sch, rq, p, enq_flags);
 }
 
-static inline void scx_reenq_reject(struct rq *rq)
-{
-	lockdep_assert_rq_held(rq);
-
-	if (scx_has_subs())
-		__scx_reenq_reject(rq);
-}
-
 #else	/* CONFIG_EXT_SUB_SCHED */
 
 static inline struct scx_sched *scx_next_descendant_pre(struct scx_sched *pos, struct scx_sched *root) { return pos ? NULL : root; }
@@ -120,7 +111,6 @@ static inline void scx_discard_ecaps_to_sync(s32 cpu, struct scx_sched_pcpu *pcp
 static inline void scx_discard_stale_ecaps_syncs(void) {}
 static inline struct scx_dispatch_q *scx_resolve_local_dsq(struct scx_sched *sch, struct rq *rq, struct task_struct *p, u64 *enq_flags) { return &rq->scx.local_dsq; }
 static inline bool scx_task_reenq_on_cap_revoke(struct rq *rq, struct task_struct *p) { return false; }
-static inline void scx_reenq_reject(struct rq *rq) {}
 static inline void scx_rescue_charge(struct rq *rq, s64 delta_exec) {}
 static inline void scx_rescue_end(struct rq *rq) {}
 static inline bool scx_rescue_keep(struct rq *rq, struct task_struct *p) { return false; }
